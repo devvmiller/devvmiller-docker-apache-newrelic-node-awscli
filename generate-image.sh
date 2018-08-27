8901818ddb9e1e1f66b24eec695d458a4f83cfe4
Gabriel Le Breton <lebreton.gabriel@gmail.com>
2018-08-26 20:03:46 -0400

    initial commit

---
+++ generate-image.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -x
+
+function get_php_node_image_from_circleci() {
+    git clone https://github.com/circleci/circleci-images
+    cd circleci-images
+    git checkout 5d28b25
+    cd php
+    ./generate-images
+    cd ../../
+    cat circleci-images/php/images/7.2.9-cli-stretch/node/Dockerfile > $1
+}
+
+get_php_node_image_from_circleci CircleCi.Dockerfile
